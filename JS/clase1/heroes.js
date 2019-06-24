d13599d074330dc934387a83e6297f117d992784
mariano <mchilano86@gmail.com>
2019-06-23 21:06:48 -0300

    Ejercicio superheroes JS

--- JS/clase1/heroes.js
+++ JS/clase1/heroes.js
@@ -1,20 +1,54 @@
+
 var IronMan = {
   nombre: "Iron Man",
   equipo: "Avengers",
   poderes: [ "Volar", "Lanzar misiles","Disparar laser"],
   energia: 100,
   getPoder: function(numero) {
-    if(numero === 0) {
-      this.energia = this.energia - 20;
+
+    if(numero === 0){
+
+      if(this.energia >= 20){
+        this.energia = this.energia - 20;
+      } else{
+
+         return false;
+      }
     }
+
+    if(numero === 1){
+
+      if(this.energia >= 45){
+        this.energia = this.energia - 45;
+      } else{
+
+         return false;
+      }
+    }
+
+    if(numero === 2){
+
+      if(this.energia >= 75){
+        this.energia = this.energia - 75;
+      } else{
+
+         return false;
+      }
+    }
+
     return this.poderes[numero];
   }
-
 }
 
-var aleatorio = Math.floor(Math.random()*3);
+var poder = true;
 
-console.log( IronMan.getPoder(aleatorio) );
+while (poder = IronMan.getPoder(Math.floor(Math.random()*3)) ) {
+
+    console.log( poder );
+
+    console.log( IronMan.energia );
+
+}
 
 
 var Hulk = {
@@ -24,11 +58,47 @@ var Hulk = {
   energia: 100,
   getPoder: function(numero) {
 
+    if(numero === 0){
+
+      if(this.energia >= 45){
+        this.energia = this.energia - 45;
+      } else{
+
+         return false;
+      }
+    }
+
+    if(numero === 1){
+
+      if(this.energia >= 15){
+        this.energia = this.energia - 15;
+      } else{
+
+         return false;
+      }
+    }
+
+    if(numero === 2){
+
+      if(this.energia >= 25){
+        this.energia = this.energia - 25;
+      } else{
+
+         return false;
+      }
+    }
+
 
     return this.poderes[numero];
   }
 }
 
-  var aleatorio = Math.floor(Math.random()*3);
+var poder = true;
 
-  console.log( Hulk.getPoder(aleatorio) );
+while (poder = Hulk.getPoder(Math.floor(Math.random()*3)) ) {
+
+    console.log( poder );
+
+    console.log( Hulk.energia );
+
+}
